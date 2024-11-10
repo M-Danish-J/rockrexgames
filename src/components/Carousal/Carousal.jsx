19be1423be5bb1997e0f8ebd83b2e5e131d460a3
imdanish <imdanishjaved@gmail.com>
2024-11-10 06:43:45 -0800

    -Slider updated

--- src/components/Carousal/Carousal.jsx
+++ src/components/Carousal/Carousal.jsx
@@ -6,11 +6,6 @@ import Img2 from '../../images/hero_slide_2.png'
 import Img3 from '../../images/hero_slide_3.png'
 import Img4 from '../../images/hero_slide_4.png'
 import Img5 from '../../images/hero_slide_5.png'
-import Img6 from '../../images/hero_slide_6.png'
-import Img7 from '../../images/hero_slide_7.png'
-import Img8 from '../../images/hero_slide_8.png'
-import Img9 from '../../images/hero_slide_9.png'
-import Img10 from '../../images/hero_slide_10.png'
 
 const contentStyle = {
     width: '100%'
@@ -23,18 +18,6 @@ export default function Carousal() {
     return (
         <>
             <Carousel afterChange={onChange} dots={false} fade={true} autoplay={true} infinite={true} speed={100}>
-            <div>
-                    <img style={contentStyle} src={Img7} alt="carousal" />
-                </div>
-                <div>
-                    <img style={contentStyle} src={Img8} alt="carousal" />
-                </div>
-                <div>
-                    <img style={contentStyle} src={Img9} alt="carousal" />
-                </div>
-                <div>
-                    <img style={contentStyle} src={Img10} alt="carousal" />
-                </div>
                 <div>
                     <img style={contentStyle} src={Img1} alt="carousal" />
                 </div>
@@ -50,10 +33,6 @@ export default function Carousal() {
                 <div>
                     <img style={contentStyle} src={Img5} alt="carousal" />
                 </div>
-                <div>
-                    <img style={contentStyle} src={Img6} alt="carousal" />
-                </div>
-               
             </Carousel>
         </>
     )
